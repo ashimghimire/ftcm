3765b4e1fd29af88af4ba3a0a716ba3e48a733e8
Mac <mac@Macs-MacBook-Air.local>
2026-05-11 20:54:06 +0545

    Add added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
   return (
     <div>
       <Menu/>
+      <section id="advertise" className="mt-20 flex items-center justify-center">
+        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fnepalicongresshq%2Fposts%2Fpfbid02ZpXqqynmKJKeymkVdoAfdLMV3BttTZDCymou62unqyJ7x8JxZhCAHcPtjGRzNaN4l&show_text=true&width=500" width="500" height="680" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+      </section>
       <section id="ft-to-cm">
        <Counter />
        </section>
